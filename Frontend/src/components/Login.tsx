12a88fb29798d034a231068e7fa0236cd273c75f
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2023-10-25 13:46:59 -0700

    set boiler plate, testing out typescript and login page

---
+++ Frontend/src/components/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Login
