9889de405be2892c11ba728f90edcaf813518db2
Peter Mora-Stevens <petermora-stevens@Peters-MacBook-Air.local>
2023-10-30 11:23:23 -0700

    added auto top sscrolling

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import ScrollTop from './components/ScrollTop';
 import Login from './routes/Login';
 import Landing from './routes/Landing';
 import Invalid from './routes/Invalid';
 import About from './routes/About';
 
+
 const App = () => {
-  return (
+
+    return (
     <BrowserRouter>
+        <ScrollTop></ScrollTop>
         <Routes>
             <Route path='/' element={<Landing/>}></Route>
                 <Route path='login' element={<Login/>}></Route>
@@ -14,7 +18,7 @@ const App = () => {
                 <Route path='about' element={<About/>}></Route>
         </Routes>
     </BrowserRouter>
-  )
+    )
 }
 
 export default App
